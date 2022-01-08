9a3c0f963ba4b8d6c9d66879b9ded8f17e1fb445
louisyoungx <1462648167@qq.com>
2022-01-09 06:13:10 +0800

    fix(save/run): fix bug build will execute code twice

--- zpylib/build/save.py
+++ zpylib/build/save.py
@@ -3,7 +3,7 @@ from .build import Build
 # 编译代码后保存
 def save(filename, target_type):
     build = Build(filename, target_type)
-    code = build.build()
+    code = build.code
     savefile(filename, code)
 
 # 保存文件
